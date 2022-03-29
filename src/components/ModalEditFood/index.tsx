a77ca1757d155ad39ef90d606d0eb4e2d22f4df0
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-29 20:24:30 -0300

    Refactor modal edit food component

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -6,39 +6,56 @@ import Modal from '../Modal';
 import Input from '../Input';
 import { FormHandles } from '@unform/core';
 
-type ModalAddFoodFormData = {
+type ModalEditFoodFormData = {
   image: string;
   name: string;
   price: string;
   description: string;
 }
 
-interface ModalAddFoodProps { 
-  isOpen: boolean;
-  setIsOpen: () => void;
-  handleAddFood: (data: ModalAddFoodFormData) => void;
- }
+interface FoodData {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
 
-function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: ModalAddFoodProps) {
+interface ModalEditFoodProps {
+  isOpen: boolean; 
+  setIsOpen: () => void; 
+  handleUpdateFood: (data: ModalEditFoodFormData) => void; 
+  editingFood: FoodData;
+}
+
+function ModalEditFood({
+  isOpen, 
+  setIsOpen, 
+  handleUpdateFood, 
+  editingFood 
+}: ModalEditFoodProps) {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = async (data: ModalAddFoodFormData) => {
-    handleAddFood(data);
+
+  const handleSubmit = async (data: ModalEditFoodFormData) => {
+    handleUpdateFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
@@ -48,4 +65,4 @@ function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: ModalAddFoodProps) {
   );
 }
 
-export default ModalAddFood;
+export default ModalEditFood;

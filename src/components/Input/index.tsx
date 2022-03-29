d66a2c1d9e191a7722bef71c1fec84b7970840e9
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-03-29 19:19:31 -0300

    Refactor input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,22 @@ import {
   useRef,
   useState,
   useCallback,
+  FC,
+  ElementType,
+  InputHTMLAttributes
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: ElementType
+}
+
+const Input: FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
